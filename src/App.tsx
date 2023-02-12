9c0d0136dc33a8e665740b5ff42aeea29a5cc38a
uch2ha <sinyavskiy.d@gmail.com>
2023-02-12 10:45:22 +0200

    ProtectedRoute for one country page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import TheContextProvider from './TheContext';
-import { BrowserRouter, Routes, Route, RouteProps } from 'react-router-dom';
+import React, { useContext } from 'react';
+import TheContextProvider, { TheContext } from './TheContext';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainPage from './components/mainPage/MainPage';
 import CountryPage from './components/countryPage/CountryPage';
 
 const App: React.FC = () => {
+  // if you try to open one country page without country data in context
+  // you will be redirected to the main page
+  const ProtectedRouteNoCounryData: React.FC<any> = (props) => {
+    const context = useContext(TheContext);
+    return context?.country ? props.children : <Navigate to='/' />;
+  };
+
   return (
     <TheContextProvider>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<MainPage />} />
-          <Route path='/country' element={<CountryPage />} />
+          <Route
+            path='/country'
+            element={
+              <ProtectedRouteNoCounryData>
+                <CountryPage />
+              </ProtectedRouteNoCounryData>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </TheContextProvider>
