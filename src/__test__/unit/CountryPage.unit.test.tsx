d11f6e0e95e5ac190118f6d3b15185c21b2b6a42
uch2ha <sinyavskiy.d@gmail.com>
2023-02-13 22:03:11 +0200

    unused import deleted

--- src/__test__/unit/CountryPage.unit.test.tsx
+++ src/__test__/unit/CountryPage.unit.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { useNavigate } from 'react-router-dom';
